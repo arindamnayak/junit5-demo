7dcdc8226468f357d8813417cf84f5b8c475dbfb
Arindam Nayak <arindam.nayak@outlook.com>
2019-08-18 23:48:56 +0530

    Sd

--- src/main/resources/util_script/pr_comment.sh
+++ src/main/resources/util_script/pr_comment.sh
@@ -5,6 +5,6 @@ cat build.out
 echo "build out done"
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Coverage: $(tail -1 build.out | sed "s/\"/'/g")\"}" \
+    -d "{\"body\": \"$(tail -5 build.out | sed -n -e 'H;${x;s/\n/,/g;s/^,//;p;}'}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
