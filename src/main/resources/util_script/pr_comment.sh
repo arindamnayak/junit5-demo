36941399f634ae22632141c8ad3f3388db4a5474
Arindam Nayak <arindamnayak@users.noreply.github.com>
2019-08-18 23:58:09 +0530

    PR comment addition
    Added facility to post coverage report as PR comments if PR is submitted.

--- src/main/resources/util_script/pr_comment.sh
+++ src/main/resources/util_script/pr_comment.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo "build out"
+cat build.out
+echo "build out done"
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Sample PR Comment\"}" \
+    -d "{\"body\": \"Coverage: $(cat build.out | grep junit)\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
