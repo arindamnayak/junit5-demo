8a72b4862442d0208a6e7fd4b70f8ff365669d95
Arindam Nayak <arindam.nayak@outlook.com>
2019-08-18 23:31:23 +0530

    update dpr comment

--- src/main/resources/util_script/pr_comment.sh
+++ src/main/resources/util_script/pr_comment.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+cat ../../../../build.out
+cat build.out
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"$(cat ../../../../build.out | sed "s/\"/'/g")\"}" \
+    -d "{\"body\": \"Coverage: $(tail -1 ../../../../build.out | sed "s/\"/'/g")\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
