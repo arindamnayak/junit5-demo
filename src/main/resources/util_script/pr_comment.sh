4a53af337bf3788efca978b710bab28fd132e235
Arindam Nayak <arindam.nayak@outlook.com>
2019-08-18 23:22:32 +0530

    added script to post coverage

--- src/main/resources/util_script/pr_comment.sh
+++ src/main/resources/util_script/pr_comment.sh
@@ -2,6 +2,6 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"Sample PR Comment\"}" \
+    -d "{\"body\": \"$(cat ../../../../build.out | sed "s/\"/'/g")\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
